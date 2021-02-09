162a5a8d1d371237e0623fa2e980a1fb0d834229
Joe <frazierj@wustl.edu>
2021-02-09 00:50:14 -0600

    Prettierrc

--- src/data-structures/Stack.ts
+++ src/data-structures/Stack.ts
@@ -2,58 +2,58 @@
  * Stack https://en.wikipedia.org/wiki/Stack_(abstract_data_type)
  */
 export default class Stack<T> {
-  private store: T[] = [];
+    private store: T[] = []
 
-  /**
-   * pushes a(n)
-   * @param item onto the top of the stack
-   */
-  push(item: T) {
-    this.store.unshift(item);
-  }
-
-  /**
-   * removes and
-   * @returns the first item in the stack
-   */
-  pop(): T | undefined {
-    return this.store.shift();
-  }
-
-  /**
-   * @returns the first item in the stack
-   */
-  peek(): T | undefined {
-    return this.store[0];
-  }
-
-  /**
-   * @returns a boolean indicating if the stack is empty
-   */
-  isEmpty(): boolean {
-    return this.store.length > 0;
-  }
-
-  /**
-   * @returns the number of items in the stack
-   */
-  getSize(): number {
-    return this.store.length;
-  }
-
-  /**
-   * @param item to search for
-   * @returns the top-most found or undefined if no item was found
-   */
-  find(item: T): T | undefined {
-    let i = 0;
-    const len = this.store.length;
-    while (i < len) {
-      if (this.store[i] === item) {
-        return this.store[i];
-      }
-      i += 1;
+    /**
+     * pushes a(n)
+     * @param item onto the top of the stack
+     */
+    push(item: T) {
+        this.store.unshift(item)
     }
-    return undefined;
-  }
+
+    /**
+     * removes and
+     * @returns the first item in the stack
+     */
+    pop(): T | undefined {
+        return this.store.shift()
+    }
+
+    /**
+     * @returns the first item in the stack
+     */
+    peek(): T | undefined {
+        return this.store[0]
+    }
+
+    /**
+     * @returns a boolean indicating if the stack is empty
+     */
+    isEmpty(): boolean {
+        return this.store.length > 0
+    }
+
+    /**
+     * @returns the number of items in the stack
+     */
+    getSize(): number {
+        return this.store.length
+    }
+
+    /**
+     * @param item to search for
+     * @returns the top-most found or undefined if no item was found
+     */
+    find(item: T): T | undefined {
+        let i = 0
+        const len = this.store.length
+        while (i < len) {
+            if (this.store[i] === item) {
+                return this.store[i]
+            }
+            i += 1
+        }
+        return undefined
+    }
 }
--- src/tests/stack.test.ts
+++ src/tests/stack.test.ts
@@ -1,55 +1,55 @@
-import Stack from "../data-structures/Stack";
+import Stack from '../data-structures/Stack'
 
-let stack: Stack<number>;
+let stack: Stack<number>
 
 beforeAll(() => {
-  stack = new Stack();
-});
+    stack = new Stack()
+})
 
-describe("my stack", () => {
-  test("is empty", () => {
-    expect(stack.isEmpty()).toBeFalsy();
-  });
+describe('my stack', () => {
+    test('is empty', () => {
+        expect(stack.isEmpty()).toBeFalsy()
+    })
 
-  test("has one item", () => {
-    stack.push(1);
-    expect(stack.getSize()).toBe(1);
-  });
+    test('has one item', () => {
+        stack.push(1)
+        expect(stack.getSize()).toBe(1)
+    })
 
-  test("has two items", () => {
-    stack.push(2);
-    expect(stack.getSize()).toBe(2);
-  });
+    test('has two items', () => {
+        stack.push(2)
+        expect(stack.getSize()).toBe(2)
+    })
 
-  test("finds bottom item", () => {
-    expect(stack.find(1)).toBe(1);
-  });
+    test('finds bottom item', () => {
+        expect(stack.find(1)).toBe(1)
+    })
 
-  test("doesn't find non-existing item", () => {
-    expect(stack.find(3)).toBeUndefined();
-  });
+    test("doesn't find non-existing item", () => {
+        expect(stack.find(3)).toBeUndefined()
+    })
 
-  test("peeks top item", () => {
-    expect(stack.peek()).toBe(2);
-  });
+    test('peeks top item', () => {
+        expect(stack.peek()).toBe(2)
+    })
 
-  test("pops top item", () => {
-    expect(stack.pop()).toBe(2);
-  });
+    test('pops top item', () => {
+        expect(stack.pop()).toBe(2)
+    })
 
-  test("pops last item", () => {
-    expect(stack.pop()).toBe(1);
-  });
+    test('pops last item', () => {
+        expect(stack.pop()).toBe(1)
+    })
 
-  test("has no items", () => {
-    expect(stack.getSize()).toBe(0);
-  });
+    test('has no items', () => {
+        expect(stack.getSize()).toBe(0)
+    })
 
-  test("peeks undefined when empty", () => {
-    expect(stack.peek()).toBeUndefined();
-  });
+    test('peeks undefined when empty', () => {
+        expect(stack.peek()).toBeUndefined()
+    })
 
-  test("pops undefined when empty", () => {
-    expect(stack.pop()).toBeUndefined();
-  });
-});
+    test('pops undefined when empty', () => {
+        expect(stack.pop()).toBeUndefined()
+    })
+})
